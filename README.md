a029687060e92fd07c91f42de970107e2fbc766b
zuprocj <jmdcorpuz@gmail.com>
2019-08-16 01:46:26 -0700

    Finsihed up the css: addded a subtle background and colors. Cleaned up the white space that I left. Fixed typos in the html and javascript in order to make the game run correctly.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # unit-4-game
+
+Created the crystal collector game according to the homework requirements.  Just like the last assignment I had trouble with the javascript.  However, this time around I was able to organize my thought process through better pseudo-coding.  The way I approached the assigment was stating what global variables I would have, then functions that I would need to have to run the game, and finally the click functions.
+
+# Getting started
+Use Gitbash to clone repo
+Go to  https://github.com/Zuprocj/unit-4-game and clone repo
+
+Also Bootstrap-powered portfolio is deployed onto Github Pages.
+https://zuprocj.github.io/unit-4-game
+
+# Built with
+VSCode
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,20 @@
-// global variables
 var wins = 0;
 var losses = 0;
-
 var crystal = {
-   ruby:
-   { 
-      value: 0 
-   },
-   diamond:
-   { 
-      value: 0
-   },
-   topaz:
-   { 
-      value: 0
-   },
-   emerald:
-   { 
-      value: 0
-   }
+   ruby : {value: 0}, 
+   diamond : {value: 0}, 
+   topaz : {value: 0}, 
+   emerald :{value: 0}
 };
-
 var score = 0;
 var target = 0;
-
 var winCount = 0;
 var lossCount = 0;
-
 var getRandom = function(min, max) {
-   return Math.floor(Math.random() * (120 - 19 + 1)) + 1;
+   return Math.floor(Math.random() * (max - min + 1)) + 1;
 }
-
 var startGame = function() {
-   var score = 0;
+   score = 0;
    target = getRandom(19, 120);
    crystal.ruby.value = getRandom(1, 12);
    crystal.diamond.value = getRandom(1, 12);
@@ -40,27 +22,39 @@ var startGame = function() {
    crystal.emerald.value = getRandom(1, 12);
    $("#score").html(score);
    $("#target").html(target);
+   console.log("target " + target);
+   console.log("ruby " + crystal.ruby.value + " | diamond " + crystal.diamond.value +
+    " | topaz " + crystal.topaz.value + " | emerald " + crystal.emerald.value)
 }
-
 var addValues = function(crystal) {
    score = score + crystal.value;
+   $("#score").html(score);
+   checkWin();
 }
-
-
+var checkWin = function() {
+   if (score > target) {
+   alert("You lost!! You collected too many crystals");
+   lossCount++;
+   $("#lossCounter").html(lossCount);
+   startGame();
+}
+   else if (score == target) {
+   alert("You win! You got the correct amount of srystals");
+   winCount++;
+   $("#winCounter").html(winCount);
+   startGame();
+   }
+}
 startGame ();
-
 $("#ruby").click(function() {
    addValues(crystal.ruby);
 });
-
 $("#diamond").click(function() {
    addValues(crystal.diamond);
 });
-
 $("#topaz").click(function() {
    addValues(crystal.topaz);
 });
-
 $("#emerald").click(function() {
    addValues(crystal.emerald);
 });
